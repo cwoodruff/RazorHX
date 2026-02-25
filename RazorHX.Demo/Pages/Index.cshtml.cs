0730a35bb9343889499b7326fe31abdc51befb30
Chris Woody Woodruff <cwoodruff@live.com>
2026-02-25 11:02:57 -0500

    Update Index.cshtml.cs

--- RazorHX.Demo/Pages/Index.cshtml.cs
+++ RazorHX.Demo/Pages/Index.cshtml.cs
@@ -8,13 +8,13 @@ public class IndexModel : PageModel
     {
         new("Actions", "cursor", 2, "/Docs/Components/Button"),
         new("Forms", "edit", 12, "/Docs/Components/Input"),
-        new("Feedback", "bell", 8, "/Feedback"),
-        new("Navigation", "arrow-right", 4, "/Tabs"),
-        new("Organization", "grid", 4, "/Organization"),
+        new("Feedback", "bell", 8, "/Docs/Components/Callout"),
+        new("Navigation", "arrow-right", 4, "/Docs/Components/Tab"),
+        new("Organization", "grid", 4, "/Docs/Components/Card"),
         new("Overlays", "layers", 3, "/Docs/Components/Dialog"),
-        new("Imagery", "image", 5, "/Imagery"),
-        new("Formatting", "clock", 4, "/Formatting"),
-        new("Utilities", "settings", 5, "/Utilities"),
+        new("Imagery", "image", 5, "/Docs/Components/Icon"),
+        new("Formatting", "clock", 4, "/Docs/Components/FormatBytes"),
+        new("Utilities", "settings", 5, "/Docs/Components/CopyButton"),
         new("Patterns", "refresh", 4, "/Patterns"),
     };
 
